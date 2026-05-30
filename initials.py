26ca268dac44ad64ce026bbaf610603cef42b33f
boatengjnr4713 <2425401993@live.gctu.edu.gh>
2026-05-30 11:42:19 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+#Description: Print out my initials BKA in large and styled letters.
+# Assignment number: 1
+#
+#Name: Boateng Kwadwo Adjabeng Junnior
+#Student ID: 2425401993
+#Email: 2425401993@live.gctu.edu.gh
+#Grader:Mr.AUgustus
+
+#On my honor, Kwadwo Adjabeng Boateng Kwadwo, this programming assignment is on own work and i have not provided this code to any student
+
 
 def main():
-  pass #code goes here
+    #Print my initials in styled large letters
 
+    print()
+    print("...BKA")
+    print()
 
+    print("...BBBBBBBBBBBB........KK........KK............AAAA.........")
+    print("...BB........BB........KK......KK.............AA..AA........")
+    print("...BB........BB........KK....KK..............AA....AA.......")
+    print("...BB........BB........KK..KK...............AA......AA......")
+    print("...BBBBBBBBBBBB........KKKK................AAAAAAAAAAAA.....")
+    print("...BBBBBBBBBBBB........KK..KK..............AA........AA.....")
+    print("...BB........BB........KK....KK............AA........AA.....")
+    print("...BB........BB........KK......KK..........AA........AA.....")
+    print("...BB........BB...**...KK........KK...**...AA........AA...**")
+    print("...BBBBBBBBBBBB...**...KK........KK...**...AA........AA...**")
+
+    print()
 
 main()
